629ddc2a9c8c9e9cdf428deb9b5ee56c85252cb7
Saurabh tewary <tewary.saurabh@gmail.com>
2018-09-02 09:13:42 +0530

    AWS Demo

--- boards/management/commands/createsu.py
+++ boards/management/commands/createsu.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Command(BaseCommand):
 
 	def handle(self, *args, **options):
-		if not User.objects.filter(username="admin").exixts():
+		if not User.objects.filter(username="admin").exists():
 			User.objects.create_superuser("admin", "admin@admin.com", "admin")
 			self.stdout.write(self.style.SUCCESS('Successfully created new superuser'))
 			
